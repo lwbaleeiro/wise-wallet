1c6b69050ea23ca0fb67326b44e5ec006c9ea3d7
lwbaleeiro <dev.baleeiro@gmail.com>
2024-02-08 14:10:50 -0300

    initial commit

---
+++ src/main/java/br/com/lwbaleeiro/wisewallet/WiseWalletApplication.java
@@ -0,0 +1,13 @@
+package br.com.lwbaleeiro.wisewallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WiseWalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WiseWalletApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/lwbaleeiro/wisewallet/WiseWalletApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lwbaleeiro.wisewallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WiseWalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
