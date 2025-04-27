d6a89b4a83d4606b3dbbdcae736b4c73c7da0a0d
dev.baleeiro <dev.baleeiro@gmail.com>
2025-04-27 20:00:14 +0000

    style css

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,9 @@
+import './global.css'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
